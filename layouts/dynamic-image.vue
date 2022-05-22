6ca6576eea0a0f40ccf168ccd0fb6ed2c952c58f
htharoldht <31093493+htharoldht@users.noreply.github.com>
2022-05-22 21:04:44 +0800

    FIXED: issue #4
    Fix the bug that the page does not update after using this layout

--- layouts/dynamic-image.vue
+++ layouts/dynamic-image.vue
@@ -1,87 +1,89 @@
 
   <template>
-  <!-- Image is at Left -->
-  <div v-if="left" class="relative h-full intro grid grid-cols-12">
-    <div class="absolute h-full w-full" style="z-index: -10">
-      <BubbleFrame v-if="!hideFrame"/>
-    </div>
-    <!-- Image -->
-    <div
-      :class="[equal ? 'col-span-6' : 'col-span-4', 'my-auto h-full']"
-      :style="{ zIndex: '-1' }"
-    >
-      <div
-        v-if="upperImage"
-        class="absolute top-1/2 transform -translate-y-1/2"
-        :class="left ? 'left-[80px]' : 'right-[80px]'"
-        :style="{
-          boxShadow: '0px 10px 30px rgba(0,0,0,0.7)',
-          zIndex: '5',
-        }"
-      >
-        <img :src="upperImage" class="w-[450px]" alt="" loading="lazy" />
+  <div class="w-full h-full"
+    <!-- Image is at Left -->
+    <div v-if="left" class="relative h-full intro grid grid-cols-12">
+      <div class="absolute h-full w-full" style="z-index: -10">
+        <BubbleFrame v-if="!hideFrame"/>
       </div>
+      <!-- Image -->
       <div
-        v-if="image"
-        class="my-auto h-full"
-        :style="upperImage ? { ...style, filter: 'blur(3px)' } : { ...style }"
-      ></div>
+        :class="[equal ? 'col-span-6' : 'col-span-4', 'my-auto h-full']"
+        :style="{ zIndex: '-1' }"
+      >
+        <div
+          v-if="upperImage"
+          class="absolute top-1/2 transform -translate-y-1/2"
+          :class="left ? 'left-[80px]' : 'right-[80px]'"
+          :style="{
+            boxShadow: '0px 10px 30px rgba(0,0,0,0.7)',
+            zIndex: '5',
+          }"
+        >
+          <img :src="upperImage" class="w-[450px]" alt="" loading="lazy" />
+        </div>
+        <div
+          v-if="image"
+          class="my-auto h-full"
+          :style="upperImage ? { ...style, filter: 'blur(3px)' } : { ...style }"
+        ></div>
+      </div>
+      <!-- Layout -->
+      <div
+        :class="[
+          'slidev-layout my-auto',
+          image ? 'col-span-6' : 'col-span-12',
+          equal ? 'col-span-6' : 'col-span-8',
+        ]"
+      >
+        <slot />
+      </div>
     </div>
-    <!-- Layout -->
-    <div
-      :class="[
-        'slidev-layout my-auto',
-        image ? 'col-span-6' : 'col-span-12',
-        equal ? 'col-span-6' : 'col-span-8',
-      ]"
-    >
-      <slot />
-    </div>
-  </div>
 
-  <!-- Image is at Right -->
-  <div v-else class="relative h-full intro grid grid-cols-12">
-    <div class="absolute h-full w-full" style="z-index: -10">
-      <backgroundBubble />
-    </div>
-    <!-- Layout -->
-    <div
-      :class="[
-        'slidev-layout my-auto',
-        image ? 'col-span-6' : 'col-span-12',
-        equal ? 'col-span-6' : 'col-span-8',
-      ]"
-    >
-      <slot />
-    </div>
-    <!-- Image -->
-    <div
-      :class="[equal ? 'col-span-6' : 'col-span-4', 'my-auto h-full']"
-      :style="{ zIndex: '-1' }"
-    >
-      <div
-        v-if="upperImage"
-        class="absolute top-1/2 transform -translate-y-1/2"
-        :class="left ? 'left-[80px]' : 'right-[80px]'"
-        :style="{
-          boxShadow: '0px 10px 30px rgba(0,0,0,0.7)',
-          zIndex: '5',
-        }"
-      >
-        <img :src="upperImage" class="w-[450px]" alt="" loading="lazy" />
+    <!-- Image is at Right -->
+    <div v-else class="relative h-full intro grid grid-cols-12">
+      <div class="absolute h-full w-full" style="z-index: -10">
+        <backgroundBubble />
       </div>
+      <!-- Layout -->
       <div
-        v-if="image"
-        class="my-auto h-full"
-        :style="
-          upperImage
-            ? {
-                ...style,
-                filter: 'blur(3px)',
-              }
-            : { ...style }
-        "
-      ></div>
+        :class="[
+          'slidev-layout my-auto',
+          image ? 'col-span-6' : 'col-span-12',
+          equal ? 'col-span-6' : 'col-span-8',
+        ]"
+      >
+        <slot />
+      </div>
+      <!-- Image -->
+      <div
+        :class="[equal ? 'col-span-6' : 'col-span-4', 'my-auto h-full']"
+        :style="{ zIndex: '-1' }"
+      >
+        <div
+          v-if="upperImage"
+          class="absolute top-1/2 transform -translate-y-1/2"
+          :class="left ? 'left-[80px]' : 'right-[80px]'"
+          :style="{
+            boxShadow: '0px 10px 30px rgba(0,0,0,0.7)',
+            zIndex: '5',
+          }"
+        >
+          <img :src="upperImage" class="w-[450px]" alt="" loading="lazy" />
+        </div>
+        <div
+          v-if="image"
+          class="my-auto h-full"
+          :style="
+            upperImage
+              ? {
+                  ...style,
+                  filter: 'blur(3px)',
+                }
+              : { ...style }
+          "
+        ></div>
+      </div>
     </div>
   </div>
 </template>
